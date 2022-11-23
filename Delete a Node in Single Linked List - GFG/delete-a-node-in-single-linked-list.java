bbc291121abc63189c7cf75a6dd7f7464c0d8198
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2022-11-23 19:29:58 +0530

    Added solution - LeetHub

---
+++ Delete a Node in Single Linked List - GFG/delete-a-node-in-single-linked-list.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+class DeleteNode
+{
+	Node head;
+	void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	public void addToTheLast(Node node) 
+	{
+		if (head == null) 
+		{
+			head = node;
+		} else 
+		{
+		   Node temp = head;
+		   while (temp.next != null)
+		   temp = temp.next;
+
+		   temp.next = node;
+		}}
+	public static void main(String args[])
+    {
+       
+         Scanner sc = new Scanner(System.in);
+		 int t  =sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			//int k = sc.nextInt();
+			DeleteNode llist = new DeleteNode();
+			//int n=Integer.parseInt(br.readLine());
+			int a1 = sc.nextInt();
+			Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			int x = sc.nextInt();
+			//System.out.println(llist.head.data);
+			  GfG g = new GfG(); 
+			//System.out.println(k);
+			//System.out.println(g.getNthFromLast(llist.head,k));
+			Node result = g.deleteNode(llist.head, x);
+			llist.printList(result);
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Linklist node structure
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+/*You are required to complete below method*/
+class GfG
+{
+    Node deleteNode(Node head, int x)
+    {
+	    // Your code here	
+	    if(head == null ){
+	        return null; // if head is null then return null
+	    }
+	    int count = 1; // if head is not null then find the index with help of count
+	    Node curr = head; // assign head to curr
+	    if(count == x){
+	        return curr.next; // if count is equal to x then return next of curr
+	    }
+	    while(curr.next != null){
+	        count++; // for each traverse increase the count
+	        if(count == x){
+	            curr.next = curr.next.next; // if count is equal to x then change the next of curr to it's next
+	        }
+	        else{
+	            curr = curr.next; // if count not matched to x then change curr to it's next
+	        }
+	    }
+	    return head; // return the head
+    }
+}
